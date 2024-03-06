71a94eb19e27771ea870a3dc0df4ab37ddbe8958
Banywick <android19377@gmail.com>
2024-03-06 14:17:13 +0300

    Проекты в сессии и a4 b a2

--- finder/utils.py
+++ finder/utils.py
@@ -6,29 +6,29 @@ from finder.models import Remains
 from django.db.models import Q, Sum
 
 
-choice_project = dict()  # Хранилище выбранных проектов
-
 
 def clear_sort(request):
-    choice_project.clear()
+    request.session['project'] = ''
     return redirect("main")
 
 
 def choice_project_dict(request):  # Словарь из выбранных проектов
-    all_project = Remains.objects.values_list(
-        "project", flat=True
-    ).distinct()  # переводит из картежа в список уникальные значения
+    all_project = Remains.objects.values_list("project", flat=True).distinct()  # переводит из картежа в список уникальные значения
     projects = request.POST.getlist("data_project")  # Выбор пользователя
+    request.session['project'] = projects
+    print(request.session.get('project'))
 
-    for i, project in enumerate(projects):
-        choice_project[i] = project  # добавляем выбор в дикт
-    return {"all_project": all_project, "project": choice_project.values()}
+    for i, projects in enumerate(projects):
+        request.session['i'] = projects
+    
+    return {"all_project": all_project, "project": request.session['project']}
 
 
 def get_context_input_filter_all(request):  # Поиск всему
     form = InputValue(request.POST)
     metiz_all = Q()  # Создаем пустой объект Q
     comment_filter = Q()
+    repl_a = Q()
     any_text = []
     if request.method == "POST":
         input_str = str(request.POST["input"])
@@ -53,7 +53,7 @@ def get_context_input_filter_all(request):  # Поиск всему
         elif input_str != "":  # метизы и текст
             values = input_str.split(" ")  # сбор значений с инпута
             values += [""] * (5 - len(values))
-            # )  # Добавляем пустые строки, если введено менее четырех слов
+            # )  # Добавляем пустые строки, если введено менее пяти слов
             for v in values:
                 if re.match(r"^(\d+(\.\d+)?[*]\d+|5f[*]\d+)$", v):
                     v1 = v.lower().replace("*", "х")  # Кириллица
@@ -61,26 +61,28 @@ def get_context_input_filter_all(request):  # Поиск всему
                     metiz_all |= (
                         Q(title__icontains=v1)
                         | Q(title__icontains=v2)
-                        | Q(title__icontains=v)
-                    )
+                        | Q(title__icontains=v))
+                elif re.match(r"^[aа][24]\b", v):
+                    r1 = v.lower().replace('а', 'a') #Меняем кирилицу на латиницу A4, A2
+                    repl_a = (Q(title__icontains=v) | Q(title__icontains=r1))
                 else:
 
                     any_text.append(v)
-            query = Q(title__icontains=any_text[0]) & Q(title__icontains=any_text[1]) & Q(title__icontains=any_text[2])
-            if any_text[3]:
-                comment_filter = Q(comment__icontains=any_text[3])
-                print(comment_filter)
+            print(any_text)
+            query = Q(title__icontains=any_text[0]) & Q(title__icontains=any_text[1])
+            if any_text[2]:
+                comment_filter = Q(comment__icontains=any_text[2])
             error_message = "Товар не найден"
 
         projects_filter_q = Q()
-        for value in choice_project.values():
+        for value in request.session.get('project'):
             projects_filter_q |= Q(
                 **{"project": value}
             )  # Динамическое создание Q по выбранным проектам
 
         remains = (
             Remains.objects.filter(projects_filter_q)
-            .filter(query & metiz_all)
+            .filter(query & metiz_all).filter(repl_a)
             .filter(comment_filter)[:100]
             | Remains.objects.filter(article__contains=input_str)
         )
@@ -94,10 +96,10 @@ def get_context_input_filter_all(request):  # Поиск всему
         return {
             "remains": remains,
             "form": form,
-            "project": choice_project.values(),
+            "project": request.session.get('project'),
         }
     else:
         return {
             "form": form,
-            "project": choice_project.values(),
+            "project": request.session.get('project'),
         }  # Возврат контест GET
