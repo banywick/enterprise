6712a888390b9b21d9d388ae8fa61e478343be00
BanyWick <android19377@gmail.com>
2024-08-23 16:41:33 +0300

    ''

--- inventory/utils/inventory_engine.py
+++ inventory/utils/inventory_engine.py
@@ -4,19 +4,26 @@ from ..models import RemainsInventory
 
 
 def get_inventory(request):
-    unic_sum_posit = RemainsInventory.objects.values('article', 'title', 'base_unit')
+    # unic_sum_posit = RemainsInventory.objects.values('article', 'title', 'base_unit')
 
 
     form = InputValue(request.POST)
     if request.method == 'POST':
+        query = Q()
         if request.POST.get('input'):
             values = request.POST['input'].split(' ')  # сбор значений с инпута
-            values += [''] * (4 - len(values))  # Добавляем пустые строки, если введено менее четырех слов
-            query = Q(title__icontains=values[0]) & Q(title__icontains=values[1]) & Q(title__icontains=values[2]) & Q(
-                title__icontains=values[3])
+            for element in values:
+                if element:
+                    query =Q(title__icontains=element)
+                    print(query)
+               
+
+           
             error_message = 'Товар не найден'
-            inventory = unic_sum_posit.filter(query) | unic_sum_posit.filter(
-                article__contains=request.POST['input'])
+            inventory = RemainsInventory.objects.filter(query)[:500] | RemainsInventory.objects.filter(
+                article__icontains=request.POST['input'])
+            # inventory = unic_sum_posit.filter(query)[:500] | unic_sum_posit.filter(
+            #     article__contains=request.POST['input'])
             if not inventory.exists():  # если ничего не найдено из нескольких значений в инпуте
                 return {'form': form, 'e_art_title': error_message}  # post если не найдено
             return {'form': form, 'inventory': inventory}  # post
